e65a66a754bd968246d364e7f110068d21e246bb
zvakanaka <8365885+zvakanaka@users.noreply.github.com>
2025-10-30 20:12:01 -0600

    uncomment

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
+-- comment this out to enable this plugin
 if true then return {} end
--- return {
---   "ellisonleao/gruvbox.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("gruvbox").setup({
---       -- transparent_mode = true, -- causes problems with bufferline.nvim (this is fixable, so maybe later)
---     })
---     vim.o.background = "dark" -- or "light" for light mode
---     vim.cmd("colorscheme gruvbox")
---   end,
--- }
+return {
+  "ellisonleao/gruvbox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      -- transparent_mode = true, -- causes problems with bufferline.nvim (this is fixable, so maybe later)
+    })
+    vim.o.background = "dark" -- or "light" for light mode
+    vim.cmd("colorscheme gruvbox")
+  end,
+}
